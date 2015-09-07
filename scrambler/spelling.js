c56139e07a23364ebaba25aecb93f3a02b83127d
m1c0l <michael.li11702@gmail.com>
2015-09-06 23:49:34 -0400

    Major logic updates

--- scrambler/spelling.js
+++ scrambler/spelling.js
@@ -1,47 +1,42 @@
-function isalpha(c) {
-	return /[a-zA-Z]/.test(c);
-}
-
-// Replace word[index] with character
-function replaceChar(word, index, character) {
-	return word.substr(0, index) + character + word.substr(index + character.length);
-}
-
 var replaceWord = function(word) {
 	// Word is unchanged if it is 3 characters or less
-	if (word.length <= 3)
+	if (word.length <= 3) {
 		return word;
-
-	// Always swap the middle characters in a 4-letter word
-	if (word.length == 4) {
-		return word[0] + word[2] + word[1] + word[3];
 	}
 
-	// Fix the position of the first, last, and non-alphabetic characters
-	var fixed = [];
-	var letters = [];
-	fixed[0] = true;
-	fixed[word.length - 1] = true;
-	for (var i = 1; i < word.length - 1; i++) {
-		if (isalpha(word[i]))
-			letters.push(word[i]);
-		else
-			fixed[i] = true;
-	}
-
-	// Replace unfixed positions with a random letter
-	for (i = 1; i < word.length - 1; i++) {
-		if (!fixed[i]) {
-			var rand = Math.floor(Math.random() * letters.length);
-			word = replaceChar(word, i, letters[rand]);
-			letters.splice(rand, 1);
+	// Test if the middle letters are all the same, e.g., "keep"
+	// If so, return the original word
+	var sameLetters = true;
+	var secondLetter = word[1];
+	for (var i = 2; sameLetters && i < word.length - 1; i++) {
+		if (word[i] != secondLetter) {
+			sameLetters = false;
 		}
 	}
+	if (sameLetters) {
+		return word;
+	}
 
-	return word;
+	// Do the Fisher-Yates shuffle until the word has changed
+	var newWord;
+	do {
+		var letters = word.split(""),
+	        n = letters.length;
+
+	    for (var i = n - 3; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var tmp = letters[i + 1];
+	        letters[i + 1] = letters[j + 1];
+	        letters[j + 1] = tmp;
+	    }
+	    newWord = letters.join("");
+	} while(newWord.localeCompare(word) === 0);
+
+	return newWord;
 };
 
 var processNodeText = function(textNode) {
+	// Skip this text node if it's inside a tag we don't want to edit
 	var ignoreTags = ['script', 'style'];
 	var parentElem = textNode.parentElement;
 	if (parentElem) {
@@ -50,12 +45,25 @@ var processNodeText = function(textNode) {
 			return;
 		}
 	}
+
 	var nodeValue = textNode.nodeValue;
-	var wordArray = nodeValue.split(/\b/);
-	var editedWords = '';
-	for (var a in wordArray) {
-		editedWords += replaceWord(wordArray[a]);
+
+	// If the text node is empty, return
+	if (!/[^\n\s]/.test(nodeValue)) {
+		return;
 	}
+
+	// Capture the non-alpha characters and split by them
+	var nonAlpha = /[^a-zA-Z]+/g;
+	var nonAlphaArray = nodeValue.match(nonAlpha);
+	var wordArray = nodeValue.split(nonAlpha);
+	// Edit the words and add the non-alpha characters in between
+	var editedWords = '';
+	for (var i = 0; i < wordArray.length - 1; i++) {
+		editedWords += replaceWord(wordArray[i]);
+		editedWords += nonAlphaArray[i];
+	}
+	editedWords += replaceWord(wordArray[wordArray.length - 1]);
 	textNode.nodeValue = editedWords;
 };
 
